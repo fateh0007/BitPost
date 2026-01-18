3f47e23b7a009632973c6278fc2e0ef316507846
fateh0007 <fatehalam123463@gmail.com>
2026-01-18 17:25:02 +0530

    Created Forms

---
+++ BitPost/tweet/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Tweet
+
+class TweetForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        fields = ['text', 'photo']
+        widgets = {
+            'text': forms.Textarea(attrs={'rows': 3, 'placeholder': 'What\'s happening?'}),
+        }
