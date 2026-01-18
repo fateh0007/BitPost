23cc3002438bdf0a55231215af8c221aac69cc44
fateh0007 <fatehalam123463@gmail.com>
2026-01-18 18:38:27 +0530

    Added edit and delete backend

--- BitPost/tweet/views.py
+++ BitPost/tweet/views.py
@@ -15,10 +15,28 @@ def tweet_create(request):
     if request.method == "POST":
         form = TweetForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save(commit=False)
-            Tweet.user = request.user
-            Tweet.save()
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
             return redirect('tweet_list')
     else:
         TweetForm()
     return render(request,"tweet_form.html", {'form': form})
+
+def tweet_edit(request,tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user) 
+    if request.method=='POST':
+        form  = TweetForm(request.POST, request.FILES,instance = tweet)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            return redirect('tweet_list')
+    else:
+        form = TweetForm(instance=tweet)
+    return render(request,"tweet_form.html", {'form': form})
+
+def tweet_delete(request,tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user).delete()
+    if request.method == "POST":
+        tweet.delete()
+        return redirect('tweet_list')
+    return render(request, "tweet_confirm_delete.html", {'tweet': tweet})
