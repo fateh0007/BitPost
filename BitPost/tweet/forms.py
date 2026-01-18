ecc200787daf545c82d02b150916806190e0ab9f
fateh0007 <fatehalam123463@gmail.com>
2026-01-18 17:26:10 +0530

    Updated Forms

--- BitPost/tweet/forms.py
+++ BitPost/tweet/forms.py
@@ -5,6 +5,4 @@ class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = ['text', 'photo']
-        widgets = {
-            'text': forms.Textarea(attrs={'rows': 3, 'placeholder': 'What\'s happening?'}),
-        }
+        
